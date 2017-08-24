4f5e2b56a5a32467a4765ccdb676a04031231e6c
Swathi555 <31124982+Swathi555@users.noreply.github.com>
2017-08-24 20:41:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 }) ;
 
-app.get('/article-two', function (req,res) {
-   res.sendFile('Article two will be served here');
+app.get('/article-one', function (req,res) {
+   res.sendFile('Article one will be served here');
 });
 
 app.get('/article-three', function (req,res) {
