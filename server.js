121e26f2f985f3b9f0bb433fc90a4104155cc56d
Swathi555 <31124982+Swathi555@users.noreply.github.com>
2017-08-24 20:41:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 }) ;
 
 app.get('/article-one', function (req,res) {
-   res.sendFile('Article one will be served here');
+   res.send('Article one will be served here');
 });
 
 app.get('/article-three', function (req,res) {
