16adda9e151b1a2e19f27f8130585baa2f54e7ed
Swathi555 <31124982+Swathi555@users.noreply.github.com>
2017-08-26 18:45:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var articleOne = {
     title:"article One | Swathi ",
     heading:"article One",
     date:"23 Aug 2017",
@@ -20,7 +20,45 @@ var content = {
            </p> `
 };
 
+function createTemplate (data) {
+     var title = data.title ;
+     var date = data.date ;
+     var heading = data.heading ;
+     var content = data.content ;
 
+
+var htmlTemplate = `<html>
+  <head>
+     <title>
+        ${title}
+     </title>
+     <meta name="viewport" content="width=device-width, initial-scale=1" />
+           <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+  <body> 
+   <div class="container">
+       <div>
+           <a href='/'>Home</a>
+       </div>
+           <hr/>
+           <h3>
+               ${heading}
+           </h3>
+           <div>
+               ${date}
+           </div>
+           <div> 
+               ${content}
+           </div>
+        </div>
+       </body>
+     </html>
+     `;
+     return htmlTemplate ;
+}
+     
+     
+     
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 }) ;
